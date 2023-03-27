431b98c0ad68bd9310b046e02c89802cc6417eb6
jqsx <101830653+jqsx@users.noreply.github.com>
2023-03-27 12:07:09 +0200

    uplod

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
